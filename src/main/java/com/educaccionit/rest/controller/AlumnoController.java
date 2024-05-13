18c54a53c0f45049e653ac77ce4776a86f3ac4e6
Cristian <saccomanno@gmail.com>
2024-05-13 20:20:12 -0300

    Agregado de archivo de log

--- src/main/java/com/educaccionit/rest/controller/AlumnoController.java
+++ src/main/java/com/educaccionit/rest/controller/AlumnoController.java
@@ -2,6 +2,8 @@ package com.educaccionit.rest.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -26,16 +28,19 @@ public class AlumnoController {
 	@Qualifier("alumnoService")
     private AlumnoService alumnoService;
 
+	private static final Logger logger = LoggerFactory.getLogger(AlumnoController.class);
 
     @GetMapping
     public ResponseEntity<List<Alumno>> getAllAlumnos() throws Exception {
         List<Alumno> alumnos = alumnoService.getAllAlumnos();
+        logger.info("Consulta de Todos los alumnos");
         return new ResponseEntity<>(alumnos, HttpStatus.OK);
     }
 
     @GetMapping("/{legajo}")
     public ResponseEntity<Alumno> getAlumnoById(@PathVariable("legajo") int legajo) throws Exception {
         Alumno alumno = alumnoService.getAlumnoById(legajo);
+        logger.info("Consulta de alumno por Id {}", legajo);
         if (alumno != null) {
             return new ResponseEntity<>(alumno, HttpStatus.OK);
         } else {
@@ -46,18 +51,22 @@ public class AlumnoController {
     @PostMapping
     public ResponseEntity<Void> addAlumno(@RequestBody Alumno alumno) throws Exception {
         alumnoService.addAlumno(alumno);
+        logger.info("Alta de alumno {}", alumno);
+        
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{legajo}")
     public ResponseEntity<Void> updateAlumno(@PathVariable("legajo") int legajo, @RequestBody Alumno alumno) throws Exception {
         alumnoService.updateAlumno(legajo, alumno);
+        logger.info("Actualizacion de alumno con lejago {}, Alumno: {}", legajo, alumno);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{legajo}")
     public ResponseEntity<Void> deleteAlumno(@PathVariable("legajo") int legajo) throws Exception {
         alumnoService.deleteAlumno(legajo);
+        logger.info("Baja de alumno con Legajo: {}", legajo);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
